0c399fdb2e808e0785a6a3dffbe038c425c3705f
Gelo <38291848+gelokatil@users.noreply.github.com>
2022-07-29 12:29:54 +0200

    Fix for translations not showing up in frontend (#760)
    * fix for translations not showing up in frontend

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -6,11 +6,15 @@ import de from "../../../translations/frontend/de.json";
 import en from "../../../translations/frontend/en.json";
 import nl from "../../../translations/frontend/nl.json";
 import pl from "../../../translations/frontend/pl.json";
+import fr from "../../../translations/frontend/fr.json";
+import es from "../../../translations/frontend/es.json";
 const messages = {
   de,
   en,
   nl,
-  pl
+  pl,
+  fr,
+  es
 };
 
 export const i18n = createI18n({
