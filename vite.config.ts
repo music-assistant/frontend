3d004c4311627815563b63267af66045830801e5
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-03-10 13:56:29 +0100

    Fix loading the Vue frontend through ingress proxy (#5)

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'./',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
