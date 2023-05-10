e6678af40759839cfba5ff0e16d2f5ca8d3802f6
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-05-10 18:17:03 +0200

    disable i18n warnings (because theyre annoying)

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,16 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createI18n } from 'vue-i18n';
+import { createI18n } from "vue-i18n";
 
 /*
  * All i18n resources specified in the plugin `include` option can be loaded
  * at once using the import syntax
  */
-import messages from '@intlify/unplugin-vue-i18n/messages'
+import messages from "@intlify/unplugin-vue-i18n/messages";
 
 export const i18n = createI18n({
   legacy: false,
   globalInjection: true,
   locale: navigator.language.split("-")[0],
-  fallbackLocale: 'en',
+  fallbackLocale: "en",
+  missingWarn: false,
+  fallbackWarn: false,
+  silentTranslationWarn: true,
   messages,
 });
