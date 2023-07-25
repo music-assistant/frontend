e3349cc6cd29283c464a719a1477c51f8ff87aca
Jonathan Bangert <jonathan@bangert.dk>
2023-07-25 11:52:29 +0200

    I broke something...

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 import { VitePWA } from 'vite-plugin-pwa';
 import { fileURLToPath, URL } from 'node:url';
 import webfontDownload from 'vite-plugin-webfont-dl';
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(async () => ({
   base: './',
   plugins: [
     vue({
@@ -52,7 +52,7 @@ export default defineConfig({
     }),
     VueI18nPlugin({
       include: [path.resolve(__dirname, './src/translations/**')],
-    }),
+    })
   ],
   define: { 'process.env': {} },
   resolve: {
@@ -71,13 +71,13 @@ export default defineConfig({
   },
   // to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ['VITE_', 'TAURI_'],
+  envPrefix: ["VITE_", "TAURI_"],
   build: {
     // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
   },
-});
+}));
