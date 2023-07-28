ede48d895b8dc517feccb028f2aa62f2f2dfab5b
Jonathan Bangert <jonathan@bangert.dk>
2023-07-28 14:17:54 +0200

    System theme setting

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,24 @@
     <v-container v-if="setup" fill-height class="d-flex align-center justify-center h-screen">
       <v-sheet width="350" class="mx-auto rounded-lg">
         <v-form style="padding-left: 15px; padding-right: 15px" @submit="start">
-          <v-card-title class="my-3">Music Assistant server details</v-card-title>
+          <v-card-title class="my-3" style="cursor: default">Music Assistant server details</v-card-title>
           <v-text-field v-model="ip" variant="outlined" label="IP / Hostname" placeholder="homeassistant.local" />
           <v-text-field v-model="port" variant="outlined" type="number" label="Port" placeholder="8095" />
-          <v-card-title class="my-3">Client settings</v-card-title>
-          <v-switch v-model="discordRPCEnabled" label="Start Discord rich presence" @change="discordRpcConfig" />
+          <v-card-title class="my-3" style="cursor: default">Client settings</v-card-title>
+          <v-switch v-model="discordRPCEnabled" label="Start Discord Rich Presence" @change="discordRpcConfig" />
           <v-switch v-model="squeezeliteEnabled" label="Start Squeezelite" @change="squeezeliteConfig" />
-          <v-switch v-model="darkThemeEnabled" label="Dark theme" @change="themeConfig" />
+          <v-card-subtitle style="cursor: default">Theme Setting</v-card-subtitle>
+          <v-btn-toggle
+            v-model="themeSetting"
+            style="width: 100%; margin-bottom: 12px"
+            mandatory
+            rounded="lg"
+            @update:model-value="themeSettingConfig"
+          >
+            <v-btn class="text-center" style="width: 35%" value="system">System</v-btn>
+            <v-btn class="text-center" style="width: 35%" value="light">Light</v-btn>
+            <v-btn class="text-center" style="width: 35%" value="dark">Dark</v-btn>
+          </v-btn-toggle>
           <v-btn type="submit" block class="mb-5" text="Start" />
         </v-form>
       </v-sheet>
@@ -25,32 +36,16 @@ import { useTheme } from 'vuetify';
 import { store } from './plugins/store';
 import { ColorCoverPalette, getContrastingTextColor } from '@/helpers/utils';
 import { invoke } from '@tauri-apps/api/tauri';
+import { appWindow } from '@tauri-apps/api/window';
 
 const setup = ref(true);
 const discordRPCEnabled = ref(false);
 const squeezeliteEnabled = ref(false);
-const darkThemeEnabled = ref(false);
 const port = ref(8095);
 const ip = ref('homeassistant.local');
+const themeSetting = ref('light');
 
-// methods
-const discordRpcConfig = () => {
-  localStorage.setItem('discordRPCEnabled', discordRPCEnabled.value.toString());
-};
-
-const squeezeliteConfig = () => {
-  localStorage.setItem('squeezeliteEnabled', squeezeliteEnabled.value.toString());
-};
-
-const themeConfig = () => {
-  console.log(darkThemeEnabled.value.toString());
-  localStorage.setItem('darkThemeEnabled', darkThemeEnabled.value.toString());
-  if (darkThemeEnabled.value.toString() == 'true') {
-    theme.global.name.value = 'dark';
-  } else {
-    theme.global.name.value = 'light';
-  }
-};
+let systemTheme = 'light';
 
 const theme = useTheme();
 let lightTheme = theme.themes.value.light;
@@ -68,6 +63,26 @@ const themeColor = function (colors: ColorCoverPalette) {
   darkTheme.colors['secondary'] = colors.lightColor || '#ff9800';
   darkTheme.colors['on-secondary'] = getContrastingTextColor(colors.lightColor) || '#fff';
   darkTheme.variables['medium-emphasis-opacity'] = 1;
+};
+
+// methods
+const discordRpcConfig = () => {
+  localStorage.setItem('discordRPCEnabled', discordRPCEnabled.value.toString());
+};
+
+const themeSettingConfig = () => {
+  localStorage.setItem('themeSetting', themeSetting.value);
+  if (themeSetting.value == 'dark') {
+    theme.global.name.value = 'dark';
+  } else if (themeSetting.value == 'light') {
+    theme.global.name.value = 'light';
+  } else {
+    theme.global.name.value = systemTheme;
+  }
+};
+
+const squeezeliteConfig = () => {
+  localStorage.setItem('squeezeliteEnabled', squeezeliteEnabled.value.toString());
 };
 
 // computed properties
@@ -109,24 +124,42 @@ watch(
   },
 );
 
-onMounted(() => {
+onMounted(async () => {
   // Set to previus settings
   let ip_storage = localStorage.getItem('mass_ip') || 'homeassistant.local';
   let port_storage = Number(localStorage.getItem('mass_port')) || 8095;
   let start_discord_rpc = localStorage.getItem('discordRPCEnabled') === 'true' || false;
   let start_squeezelite = localStorage.getItem('squeezeliteEnabled') === 'true' || false;
-  let dark_theme = localStorage.getItem('darkThemeEnabled') === 'true' || false;
+  let theme_setting = localStorage.getItem('themeSetting') || 'system';
 
   discordRPCEnabled.value = start_discord_rpc;
   squeezeliteEnabled.value = start_squeezelite;
-  darkThemeEnabled.value = dark_theme;
   ip.value = ip_storage;
   port.value = port_storage;
+  themeSetting.value = theme_setting;
 
-  themeConfig();
+  // Set inital theme
+  if ((await appWindow.theme().toString()) == 'Dark') {
+    systemTheme = 'dark';
+  } else {
+    systemTheme = 'light';
+  }
+
+  console.log(await appWindow.theme());
+
+  // Update theme live
+  await appWindow.onThemeChanged(({ payload: newTheme }) => {
+    if (newTheme.toString() == 'Dark') {
+      systemTheme = 'dark';
+    } else {
+      systemTheme = 'light';
+    }
+    theme.global.name.value = systemTheme;
+  });
 });
 
 const start = () => {
+  console.log(themeSetting.value);
   // Save ip and port
   localStorage.setItem('mass_ip', ip.value);
   localStorage.setItem('mass_port', port.value.toString());
