fb2a505547bddab5efd84362566a1862cc583b39
Jonathan Bangert <jonathan@bangert.dk>
2023-09-29 22:56:54 +0200

    Add relaunch button

--- src/views/settings/ClientConfigs.vue
+++ src/views/settings/ClientConfigs.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- client settings -->
-  <div style="margin-bottom: 10px">
-    <v-toolbar color="transparent" style="height: 55px">
+  <div>
+    <v-toolbar color="transparent">
       <template #title>{{ $t('settings.client') }}</template>
     </v-toolbar>
-    <Container style="overflow: hidden">
-      <v-table style="overflow: hidden">
-        <tbody style="overflow: hidden">
+    <Container>
+      <v-table>
+        <tbody>
           <tr>
             <td>Enable Discord RPC</td>
             <td>
@@ -36,7 +36,8 @@
                 v-model="themeSetting"
                 style="width: 100%"
                 mandatory
-                rounded="lg"
+                variant="outlined"
+                divided
                 @update:model-value="themeSettingConfig"
               >
                 <v-btn class="text-center my-auto" style="width: 15%; height: 80%" value="system">System</v-btn>
@@ -50,6 +51,7 @@
             <td>
               <v-text-field
                 v-model="ip"
+                style="margin-top: 15px; height: 70px"
                 variant="outlined"
                 label="IP / Hostname"
                 placeholder="homeassistant.local"
@@ -62,6 +64,7 @@
             <td>
               <v-text-field
                 v-model="port"
+                style="margin-top: 15px; height: 70px"
                 variant="outlined"
                 type="number"
                 label="Port"
@@ -77,9 +80,15 @@
             </td>
           </tr>
           <tr>
-            <td>App Version</td>
+            <td>Companion Version</td>
             <td>
               {{ version }}
+            </td>
+          </tr>
+          <tr>
+            <td>Restart Companion</td>
+            <td>
+              <v-btn @click.prevent="relaunch">Restart Companion App</v-btn>
             </td>
           </tr>
         </tbody>
@@ -95,6 +104,7 @@ import Container from '@/components/mods/Container.vue';
 import { useRouter } from 'vue-router';
 import { emit } from '@tauri-apps/api/event';
 import { getVersion } from '@tauri-apps/api/app';
+import { relaunch } from '@tauri-apps/api/process';
 
 // global refs
 const router = useRouter();
@@ -104,7 +114,7 @@ const squeezeliteEnabled = ref(false);
 const port = ref(8095);
 const ip = ref('homeassistant.local');
 const themeSetting = ref('light');
-const version = ref('loading...');
+const version = ref('Loading...');
 
 const theme = useTheme();
 
