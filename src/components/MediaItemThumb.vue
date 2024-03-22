67b4866cbf5f4adebbefbc499869f98932f9d81f
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-03-22 02:53:11 +0000

    WIP: Use native lazy images to avoid the additional JS lazy loading being done by vuetify

--- src/components/MediaItemThumb.vue
+++ src/components/MediaItemThumb.vue
@@ -1,49 +1,41 @@
 <template>
-  <v-img
+  <img
+    loading="lazy"
+    width="200px"
+    height="200px"
+    :src="imgData"
+    :class="{ rounded: rounded }"
+    :style="lazyStyle"
+  />
+  <!-- <v-img
     :key="item && 'uri' in item! ? item?.uri : item?.queue_item_id"
-    :style="`height:${size || height}px; width:${size || width}px; ${
-      rounded ? 'border-radius: 4px;' : ''
-    }`"
     :cover="cover"
     :src="imgData"
     :aspect-ratio="aspectRatio"
-    :lazy-src="
-      !lazySrc
-        ? $vuetify.theme.current.dark
-          ? imgCoverDark
-          : imgCoverLight
-        : lazySrc
-    "
     @error="
       () => {
         imgData = fallbackImage;
       }
     "
-  >
-    <template #placeholder>
-      <div class="d-flex align-center justify-center fill-height">
-        <v-progress-circular indeterminate />
-      </div>
-    </template>
-  </v-img>
+  /> -->
 </template>
 
 <script setup lang="ts">
-import { watch, ref, computed } from 'vue';
+import { watch, ref, computed } from "vue";
 import type {
   ItemMapping,
   MediaItemImage,
   MediaItemType,
   QueueItem,
-} from '@/plugins/api/interfaces';
-import { ImageType, MediaType } from '@/plugins/api/interfaces';
-import { api } from '@/plugins/api';
-import { useTheme } from 'vuetify';
+} from "@/plugins/api/interfaces";
+import { ImageType, MediaType } from "@/plugins/api/interfaces";
+import { api } from "@/plugins/api";
+import { useTheme } from "vuetify";
 import {
   imgCoverDark,
   imgCoverLight,
   iconFolder,
-} from '@/components/QualityDetailsBtn.vue';
+} from "@/components/QualityDetailsBtn.vue";
 
 export interface Props {
   item?: MediaItemType | ItemMapping | QueueItem;
@@ -61,9 +53,9 @@ export interface Props {
 const props = withDefaults(defineProps<Props>(), {
   item: undefined,
   size: undefined,
-  width: '100%',
-  height: 'auto',
-  aspectRatio: '1/1',
+  width: "100%",
+  height: "auto",
+  aspectRatio: "1/1",
   cover: true,
   fallback: undefined,
   thumb: true,
@@ -71,29 +63,14 @@ const props = withDefaults(defineProps<Props>(), {
   rounded: true,
 });
 
-const imgData = ref<string>();
 const theme = useTheme();
+const lazyStyle = {
+  backgroundImage: `url(${theme.current.value.dark ? imgCoverDark : imgCoverLight})`,
+};
 
-const fallbackImage = computed(() => {
-  if (props.fallback) return props.fallback;
-  if (
-    props.item &&
-    'media_type' in props.item &&
-    props.item.media_type == MediaType.FOLDER
-  )
-    return iconFolder;
-  if (!props.item) return '';
-  if (!props.item.name) return '';
-  return getAvatarImage(
-    props.item.name,
-    theme.current.value.dark,
-    thumbSize.value,
-  );
-});
-
-const thumbSize = computed(() => {
-  if (typeof props.size == 'number') return props.size;
-  else if (typeof props.width == 'number' && typeof props.height == 'number') {
+function getThumbSize() {
+  if (typeof props.size == "number") return props.size;
+  else if (typeof props.width == "number" && typeof props.height == "number") {
     if (props.height > props.width) {
       return props.height;
     } else {
@@ -101,19 +78,45 @@ const thumbSize = computed(() => {
     }
   } else if (props.thumb) return 256;
   return 0;
-});
+}
+const thumbSize = getThumbSize();
 
-watch(
-  () => props.item,
-  async (newVal) => {
-    if (newVal) {
-      imgData.value =
-        getImageThumbForItem(newVal, ImageType.THUMB, thumbSize.value) ||
-        fallbackImage.value;
-    } else imgData.value = fallbackImage.value;
-  },
-  { immediate: true },
+function getFallbackImage() {
+  if (props.fallback) return props.fallback;
+  if (
+    props.item &&
+    "media_type" in props.item &&
+    props.item.media_type == MediaType.FOLDER
+  )
+    return iconFolder;
+  if (!props.item) return "";
+  if (!props.item.name) return "";
+  return getAvatarImage(
+    props.item.name,
+    theme.current.value.dark,
+    thumbSize.value,
+  );
+}
+const fallbackImage = getFallbackImage();
+
+const imgData = ref(
+  props.item
+    ? getImageThumbForItem(props.item, ImageType.THUMB, thumbSize) ||
+        fallbackImage
+    : fallbackImage,
 );
+
+// watch(
+//   () => props.item,
+//   async (newVal) => {
+//     if (newVal) {
+//       imgData.value =
+//         getImageThumbForItem(newVal, ImageType.THUMB, thumbSize.value) ||
+//         fallbackImage.value;
+//     } else imgData.value = fallbackImage.value;
+//   },
+//   { immediate: true },
+// );
 </script>
 
 <script lang="ts">
@@ -143,45 +146,45 @@ export const getMediaItemImage = function (
   // get imageurl for mediaItem
   if (!mediaItem) return undefined;
   // handle image in queueitem
-  if ('image' in mediaItem && mediaItem.image) return mediaItem.image;
+  if ("image" in mediaItem && mediaItem.image) return mediaItem.image;
   // handle regular image within mediaitem
-  if ('metadata' in mediaItem && mediaItem.metadata.images) {
+  if ("metadata" in mediaItem && mediaItem.metadata.images) {
     for (const img of mediaItem.metadata.images) {
-      if (img.provider == 'http' && !includeFileBased) continue;
+      if (img.provider == "http" && !includeFileBased) continue;
       if (img.type == type) return img;
     }
   }
   // prefer album image in case of tracks
   if (
-    'album' in mediaItem &&
+    "album" in mediaItem &&
     mediaItem.album &&
-    'metadata' in mediaItem.album &&
+    "metadata" in mediaItem.album &&
     mediaItem.album.metadata &&
     mediaItem.album.metadata.images
   ) {
     for (const img of mediaItem.album.metadata.images) {
-      if (img.provider == 'http' && !includeFileBased) continue;
+      if (img.provider == "http" && !includeFileBased) continue;
       if (img.type == type) return img;
     }
   }
   // retry with album artist
   if (
-    'artist' in mediaItem &&
-    'metadata' in mediaItem.artist &&
+    "artist" in mediaItem &&
+    "metadata" in mediaItem.artist &&
     mediaItem.artist.metadata &&
     mediaItem.artist.metadata.images
   ) {
     for (const img of mediaItem.artist.metadata.images) {
-      if (img.provider == 'http' && !includeFileBased) continue;
+      if (img.provider == "http" && !includeFileBased) continue;
       if (img.type == type) return img;
     }
   }
   // retry with track artist
-  if ('artists' in mediaItem && mediaItem.artists) {
+  if ("artists" in mediaItem && mediaItem.artists) {
     for (const artist of mediaItem.artists) {
-      if ('metadata' in artist && artist.metadata.images) {
+      if ("metadata" in artist && artist.metadata.images) {
         for (const img of artist.metadata.images) {
-          if (img.provider == 'http' && !includeFileBased) continue;
+          if (img.provider == "http" && !includeFileBased) continue;
           if (img.type == type) return img;
         }
       }
@@ -195,17 +198,17 @@ export const getImageThumbForItem = function (
   size?: number,
 ): string | undefined {
   if (!mediaItem) return;
-  let imageUrl = '';
+  let imageUrl = "";
   // find image in mediaitem
   const img = getMediaItemImage(mediaItem, type, true);
   if (!img || !img.path) return undefined;
-  if (img.provider !== 'url') {
+  if (img.provider !== "url") {
     // use imageproxy for embedded images
-    if (!api.providers[img.provider]?.available && img.provider != 'file')
+    if (!api.providers[img.provider]?.available && img.provider != "file")
       return undefined;
     const encUrl = encodeURIComponent(encodeURIComponent(img.path));
     const checksum =
-      'metadata' in mediaItem ? mediaItem.metadata?.checksum : '';
+      "metadata" in mediaItem ? mediaItem.metadata?.checksum : "";
     imageUrl = `${api.baseUrl}/imageproxy?path=${encUrl}&provider=${img.provider}&checksum=${checksum}`;
   } else {
     imageUrl = img.path;
@@ -213,7 +216,7 @@ export const getImageThumbForItem = function (
 
   if (!size) {
     return imageUrl;
-  } else if (imageUrl.includes('imageproxy')) {
+  } else if (imageUrl.includes("imageproxy")) {
     return imageUrl + `&size=${size}`;
   } else {
     // get url to resized image(thumb) from weserv service
@@ -222,9 +225,18 @@ export const getImageThumbForItem = function (
 };
 </script>
 
-<style>
+<style scoped>
 .v-avatar.v-avatar--density-default {
   height: 100% !important;
   width: 100% !important;
+}
+img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+  background-size: cover;
+}
+img.rounded {
+  border-radius: 4px;
 }
 </style>
