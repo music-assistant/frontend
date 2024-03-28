62d1931ff52d355e46f6fcecad9881c139236835
Jefferson Bledsoe <me@jeffersonbledsoe.com>
2024-03-28 22:28:34 +0000

    Reduce loading jank on home screen (#415)

--- src/components/MediaItemThumb.vue
+++ src/components/MediaItemThumb.vue
@@ -1,49 +1,33 @@
+<!-- TODO: Restore fallback image based on media type -->
+<!-- TODO: Restore fallback image based on media type -->
 <template>
-  <v-img
-    :key="item && 'uri' in item! ? item?.uri : item?.queue_item_id"
-    :style="`height:${size || height}px; width:${size || width}px; ${
-      rounded ? 'border-radius: 4px;' : ''
-    }`"
-    :cover="cover"
+  <img
+    ref="imageTag"
+    :loading="$props.lazy"
+    :height="$props.size || $props.height || '600px'"
+    :width="$props.size || $props.width || '600px'"
     :src="imgData"
-    :aspect-ratio="aspectRatio"
-    :lazy-src="
-      !lazySrc
-        ? $vuetify.theme.current.dark
-          ? imgCoverDark
-          : imgCoverLight
-        : lazySrc
-    "
-    @error="
-      () => {
-        imgData = fallbackImage;
-      }
-    "
-  >
-    <template #placeholder>
-      <div class="d-flex align-center justify-center fill-height">
-        <v-progress-circular indeterminate />
-      </div>
-    </template>
-  </v-img>
+    :class="{ rounded: rounded }"
+    :style="lazyStyle"
+  />
 </template>
 
 <script setup lang="ts">
-import { watch, ref, computed } from 'vue';
+import { computed, ref, onMounted } from "vue";
 import type {
   ItemMapping,
   MediaItemImage,
   MediaItemType,
   QueueItem,
-} from '@/plugins/api/interfaces';
-import { ImageType, MediaType } from '@/plugins/api/interfaces';
-import { api } from '@/plugins/api';
-import { useTheme } from 'vuetify';
+} from "@/plugins/api/interfaces";
+import { ImageType, MediaType } from "@/plugins/api/interfaces";
+import { api } from "@/plugins/api";
+import { useTheme } from "vuetify";
 import {
   imgCoverDark,
   imgCoverLight,
   iconFolder,
-} from '@/components/QualityDetailsBtn.vue';
+} from "@/components/QualityDetailsBtn.vue";
 
 export interface Props {
   item?: MediaItemType | ItemMapping | QueueItem;
@@ -54,46 +38,31 @@ export interface Props {
   cover?: boolean;
   fallback?: string;
   thumb?: boolean;
-  lazySrc?: string;
+  lazy?: HTMLImageElement['loading'];
   rounded?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
   item: undefined,
   size: undefined,
-  width: '100%',
-  height: 'auto',
-  aspectRatio: '1/1',
+  width: "100%",
+  height: "auto",
+  aspectRatio: "1/1",
   cover: true,
   fallback: undefined,
   thumb: true,
-  lazySrc: undefined,
+  lazy: 'lazy',
   rounded: true,
 });
 
-const imgData = ref<string>();
 const theme = useTheme();
+const lazyStyle = {
+  backgroundImage: `url(${theme.current.value.dark ? imgCoverDark : imgCoverLight})`,
+};
 
-const fallbackImage = computed(() => {
-  if (props.fallback) return props.fallback;
-  if (
-    props.item &&
-    'media_type' in props.item &&
-    props.item.media_type == MediaType.FOLDER
-  )
-    return iconFolder;
-  if (!props.item) return '';
-  if (!props.item.name) return '';
-  return getAvatarImage(
-    props.item.name,
-    theme.current.value.dark,
-    thumbSize.value,
-  );
-});
-
-const thumbSize = computed(() => {
-  if (typeof props.size == 'number') return props.size;
-  else if (typeof props.width == 'number' && typeof props.height == 'number') {
+function getThumbSize() {
+  if (typeof props.size == "number") return props.size;
+  else if (typeof props.width == "number" && typeof props.height == "number") {
     if (props.height > props.width) {
       return props.height;
     } else {
@@ -101,19 +70,50 @@ const thumbSize = computed(() => {
     }
   } else if (props.thumb) return 256;
   return 0;
-});
+}
+const thumbSize = getThumbSize();
 
-watch(
-  () => props.item,
-  async (newVal) => {
-    if (newVal) {
-      imgData.value =
-        getImageThumbForItem(newVal, ImageType.THUMB, thumbSize.value) ||
-        fallbackImage.value;
-    } else imgData.value = fallbackImage.value;
-  },
-  { immediate: true },
+function getFallbackImage() {
+  if (props.fallback) return props.fallback;
+  if (
+    props.item &&
+    "media_type" in props.item &&
+    props.item.media_type == MediaType.FOLDER
+  )
+    return iconFolder;
+  if (!props.item) return "";
+  if (!props.item.name) return "";
+  return getAvatarImage(props.item.name, theme.current.value.dark, thumbSize);
+}
+const fallbackImage = getFallbackImage();
+
+const imgData = computed(() =>
+  props.item
+    ? getImageThumbForItem(props.item, ImageType.THUMB, thumbSize) ||
+      fallbackImage
+    : fallbackImage,
 );
+
+// Remove background images
+const imageTag = ref(null);
+onMounted(() => {
+  const callback: IntersectionObserverCallback = (entries) => {
+    entries.forEach((entry) => {
+      const element = entry.target as HTMLImageElement;
+      if (element.complete) {
+        element.style.backgroundImage = '';
+        observer.disconnect();
+      }
+    });
+  };
+  const observer = new IntersectionObserver(callback, {
+    root: null,
+    threshold: 1.0,
+  });
+  if (imageTag.value) {
+    observer.observe(imageTag.value);
+  }
+});
 </script>
 
 <script lang="ts">
@@ -143,45 +143,45 @@ export const getMediaItemImage = function (
   // get imageurl for mediaItem
   if (!mediaItem) return undefined;
   // handle image in queueitem
-  if ('image' in mediaItem && mediaItem.image) return mediaItem.image;
+  if ("image" in mediaItem && mediaItem.image) return mediaItem.image;
   // handle regular image within mediaitem
-  if ('metadata' in mediaItem && mediaItem.metadata.images) {
+  if ("metadata" in mediaItem && mediaItem.metadata.images) {
     for (const img of mediaItem.metadata.images) {
-      if (img.provider == 'http' && !includeFileBased) continue;
+      if (img.provider == "http" && !includeFileBased) continue;
       if (img.type == type) return img;
     }
   }
   // prefer album image in case of tracks
   if (
-    'album' in mediaItem &&
+    "album" in mediaItem &&
     mediaItem.album &&
-    'metadata' in mediaItem.album &&
+    "metadata" in mediaItem.album &&
     mediaItem.album.metadata &&
     mediaItem.album.metadata.images
   ) {
     for (const img of mediaItem.album.metadata.images) {
-      if (img.provider == 'http' && !includeFileBased) continue;
+      if (img.provider == "http" && !includeFileBased) continue;
       if (img.type == type) return img;
     }
   }
   // retry with album artist
   if (
-    'artist' in mediaItem &&
-    'metadata' in mediaItem.artist &&
+    "artist" in mediaItem &&
+    "metadata" in mediaItem.artist &&
     mediaItem.artist.metadata &&
     mediaItem.artist.metadata.images
   ) {
     for (const img of mediaItem.artist.metadata.images) {
-      if (img.provider == 'http' && !includeFileBased) continue;
+      if (img.provider == "http" && !includeFileBased) continue;
       if (img.type == type) return img;
     }
   }
   // retry with track artist
-  if ('artists' in mediaItem && mediaItem.artists) {
+  if ("artists" in mediaItem && mediaItem.artists) {
     for (const artist of mediaItem.artists) {
-      if ('metadata' in artist && artist.metadata.images) {
+      if ("metadata" in artist && artist.metadata.images) {
         for (const img of artist.metadata.images) {
-          if (img.provider == 'http' && !includeFileBased) continue;
+          if (img.provider == "http" && !includeFileBased) continue;
           if (img.type == type) return img;
         }
       }
@@ -195,17 +195,17 @@ export const getImageThumbForItem = function (
   size?: number,
 ): string | undefined {
   if (!mediaItem) return;
-  let imageUrl = '';
+  let imageUrl = "";
   // find image in mediaitem
   const img = getMediaItemImage(mediaItem, type, true);
   if (!img || !img.path) return undefined;
-  if (img.provider !== 'url') {
+  if (img.provider !== "url") {
     // use imageproxy for embedded images
-    if (!api.providers[img.provider]?.available && img.provider != 'file')
+    if (!api.providers[img.provider]?.available && img.provider != "file")
       return undefined;
     const encUrl = encodeURIComponent(encodeURIComponent(img.path));
     const checksum =
-      'metadata' in mediaItem ? mediaItem.metadata?.checksum : '';
+      "metadata" in mediaItem ? mediaItem.metadata?.checksum : "";
     imageUrl = `${api.baseUrl}/imageproxy?path=${encUrl}&provider=${img.provider}&checksum=${checksum}`;
   } else {
     imageUrl = img.path;
@@ -213,7 +213,7 @@ export const getImageThumbForItem = function (
 
   if (!size) {
     return imageUrl;
-  } else if (imageUrl.includes('imageproxy')) {
+  } else if (imageUrl.includes("imageproxy")) {
     return imageUrl + `&size=${size}`;
   } else {
     // get url to resized image(thumb) from weserv service
@@ -222,9 +222,19 @@ export const getImageThumbForItem = function (
 };
 </script>
 
-<style>
+<style scoped>
 .v-avatar.v-avatar--density-default {
   height: 100% !important;
   width: 100% !important;
+}
+img {
+  min-width: 100%;
+  max-width: 100%;
+  height: auto;
+  display: block;
+  background-size: cover;
+}
+img.rounded {
+  border-radius: 4px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,265 +1,21 @@
 <template>
   <div>
     <Container variant="panel">
-      <!-- now playing widget row -->
-      <div v-if="nowPlayingWidgetRow.length" class="widget-row">
-        <v-toolbar color="transparent" style="width: fit-content">
-          <template #prepend><v-icon icon="mdi-playlist-play" /></template>
-          <template #title>
-            <span class="mr-3">{{ $t('currently_playing') }}</span>
-          </template>
-        </v-toolbar>
-        <carousel>
-          <swiper-slide
-            v-for="queue in nowPlayingWidgetRow"
-            :key="queue.queue_id"
-            style="min-width: 300px; max-width: 500px"
-          >
-            <PanelviewPlayerCard
-              :queue="queue"
-              @click="playerQueueClicked(queue)"
-            />
-          </swiper-slide>
-        </carousel>
-      </div>
-      <!-- all other widget rows -->
-      <div v-for="widgetRow in widgetRows" :key="widgetRow.label">
-        <div v-if="widgetRow.items.length" class="widget-row">
-          <v-toolbar
-            color="transparent"
-            :style="
-              widgetRow.path
-                ? 'cursor: pointer; width: fit-content;'
-                : 'width: fit-content;'
-            "
-            @click="widgetRow.path ? $router.replace(widgetRow.path) : ''"
-          >
-            <template #prepend><v-icon :icon="widgetRow.icon" /></template>
-            <template #title>
-              <v-badge
-                v-if="widgetRow.count"
-                inline
-                color="grey"
-                :content="widgetRow.count"
-              >
-                <span class="mr-3">{{ $t(widgetRow.label) }}</span>
-              </v-badge>
-              <template v-else>
-                <span class="mr-3">{{ $t(widgetRow.label) }}</span>
-              </template>
-            </template>
-          </v-toolbar>
-          <carousel>
-            <swiper-slide v-for="item in widgetRow.items" :key="item.uri">
-              <PanelviewItem
-                :item="item"
-                :show-checkboxes="false"
-                :show-track-number="false"
-                :is-selected="false"
-                @click="itemClicked"
-              />
-            </swiper-slide>
-          </carousel>
+      <Suspense>
+        <div>
+          <HomeCurrentlyPlayingRow />
+          <HomeWidgetRows />
         </div>
-      </div>
+        <template #fallback><v-progress-circular indeterminate /> </template>
+      </Suspense>
     </Container>
   </div>
 </template>
 
 <script setup lang="ts">
-import api from '@/plugins/api';
-import {
-  BrowseFolder,
-  MediaItemType,
-  MediaType,
-  PlayerState,
-  PlayerQueue,
-} from '@/plugins/api/interfaces';
-import PanelviewItem from '@/components/PanelviewItem.vue';
-import Carousel from '@/components/Carousel.vue';
-import { computed, onMounted, ref } from 'vue';
-import { store } from '@/plugins/store';
-import { eventbus } from '@/plugins/eventbus';
-import { itemIsAvailable } from '@/helpers/contextmenu';
-import router from '@/plugins/router';
-import PanelviewPlayerCard from '@/components/PanelviewPlayerCard.vue';
-import Container from '@/components/mods/Container.vue';
-
-interface WidgetRow {
-  label: string;
-  icon: string;
-  path?: string;
-  items: MediaItemType[];
-  count?: number;
-  queues?: PlayerQueue[];
-}
-
-const widgetRows = ref<Record<string, WidgetRow>>({
-  recently_played: {
-    label: 'recently_played',
-    icon: 'mdi-motion-play',
-    items: [],
-  },
-  artists: {
-    label: 'artists',
-    icon: 'mdi-account-music',
-    path: '/artists',
-    items: [],
-  },
-  albums: {
-    label: 'albums',
-    icon: 'mdi-album',
-    path: '/albums',
-    items: [],
-  },
-  playlists: {
-    label: 'playlists',
-    icon: 'mdi-playlist-music',
-    path: '/playlists',
-    items: [],
-  },
-  tracks: {
-    label: 'tracks',
-    icon: 'mdi-file-music',
-    path: '/tracks',
-    items: [],
-  },
-  radios: {
-    label: 'radios',
-    icon: 'mdi-radio',
-    path: '/radios',
-    items: [],
-  },
-  browse: {
-    label: 'browse',
-    icon: 'mdi-folder',
-    path: '/browse',
-    items: [],
-  },
-});
-
-onMounted(async () => {
-  // recently played widget row
-  api.getRecentlyPlayedItems(20).then((items) => {
-    widgetRows.value.recently_played.items = items;
-  });
-  // library artists widget row
-  api
-    .getLibraryArtists(undefined, undefined, 20, undefined, 'RANDOM()')
-    .then((pagedItems) => {
-      widgetRows.value.artists.items = pagedItems.items;
-      widgetRows.value.artists.count = pagedItems.total;
-    });
-
-  // library albums widget row
-  api
-    .getLibraryAlbums(
-      undefined,
-      undefined,
-      20,
-      undefined,
-      'timestamp_added DESC',
-    )
-    .then((pagedItems) => {
-      widgetRows.value.albums.items = pagedItems.items;
-      widgetRows.value.albums.count = pagedItems.total;
-    });
-
-  // library playlist widget row
-  api
-    .getLibraryPlaylists(
-      undefined,
-      undefined,
-      20,
-      undefined,
-      'timestamp_added DESC',
-    )
-    .then((pagedItems) => {
-      widgetRows.value.playlists.items = pagedItems.items;
-      widgetRows.value.playlists.count = pagedItems.total;
-    });
-
-  // library radios widget row
-  api
-    .getLibraryRadios(
-      undefined,
-      undefined,
-      20,
-      undefined,
-      'timestamp_added DESC',
-    )
-    .then((pagedItems) => {
-      widgetRows.value.radios.items = pagedItems.items;
-      widgetRows.value.radios.count = pagedItems.total;
-    });
-  // tracks widget
-  api
-    .getLibraryTracks(
-      undefined,
-      undefined,
-      20,
-      undefined,
-      'timestamp_added DESC',
-    )
-    .then((pagedItems) => {
-      widgetRows.value.tracks.items = pagedItems.items;
-      widgetRows.value.tracks.count = pagedItems.total;
-    });
-  // browse widget
-  await api.browse('', (data: MediaItemType[]) => {
-    widgetRows.value.browse.items.push(...data);
-  });
-});
-
-const playerStateOrder = {
-  [PlayerState.PLAYING]: 1,
-  [PlayerState.PAUSED]: 2,
-  [PlayerState.IDLE]: 2,
-} as const;
-
-const nowPlayingWidgetRow = computed(() => {
-  return Object.values(api.queues)
-    .filter(
-      (x) =>
-        x.active &&
-        x.items > 0 &&
-        x.queue_id in api.players &&
-        api.players[x.queue_id].powered,
-    )
-    .sort((a, b) => a.display_name.localeCompare(b.display_name))
-    .sort((a, b) => playerStateOrder[a.state] - playerStateOrder[b.state]);
-});
-
-const itemClicked = function (mediaItem: MediaItemType) {
-  if (
-    itemIsAvailable(mediaItem) &&
-    ['artist', 'album', 'playlist'].includes(mediaItem.media_type)
-  ) {
-    router.push({
-      name: mediaItem.media_type,
-      params: {
-        itemId: mediaItem.item_id,
-        provider: mediaItem.provider,
-      },
-    });
-  } else if (mediaItem.media_type === MediaType.FOLDER) {
-    router.push({
-      name: 'browse',
-      query: { path: (mediaItem as BrowseFolder).path },
-    });
-  } else {
-    eventbus.emit('playdialog', {
-      items: [mediaItem],
-      showContextMenuItems: true,
-    });
-  }
-};
-
-const playerQueueClicked = function (queue: PlayerQueue) {
-  if (queue && queue.queue_id in api.players) {
-    store.selectedPlayerId = queue.queue_id;
-  }
-};
+import Container from "@/components/mods/Container.vue";
+import HomeWidgetRows from "@/components/HomeWidgetRows.vue";
+import HomeCurrentlyPlayingRow from "@/components/HomeCurrentlyPlayingRow.vue";
 </script>
 
 <style>
@@ -291,5 +47,12 @@ const playerQueueClicked = function (queue: PlayerQueue) {
 
 .v-slide-group__next.v-slide-group__next--disabled {
   visibility: hidden;
+}
+</style>
+
+<style scoped>
+.v-progress-circular {
+  display: block;
+  margin-inline: auto;
 }
 </style>
