6e1d73aabb1d77a5b8510c6c2cc990eca83d3fb3
Christian Bianchini <cbianchini@cadosecurity.com>
2024-05-29 19:43:37 +0100

    add first stage

--- src/App.vue
+++ src/App.vue
@@ -55,6 +55,76 @@ onMounted(() => {
   window
     .matchMedia('(prefers-color-scheme: dark)')
     .addEventListener('change', setTheme);
+
+  const handleKeyDown = (e) => {
+   // console.log(e);
+      const key = e.code;
+      const ctrlPressed = e.ctrlKey;
+      const altPressed = e.altKey;
+      const shiftPressed = e.shiftKey;
+      let keyDetected = true;
+
+      if (ctrlPressed) {
+          if (altPressed) {
+              if (key === 'KeyS') api.queueCommandShuffle(
+                                    store.activePlayerQueue?.queue_id || '',
+                                    store.activePlayerQueue?.shuffle_enabled ? false : true,
+                                  )
+              else if (key === 'KeyR') api.queueCommandRepeat(
+                                          store.activePlayerQueue?.queue_id || '',
+                                          getValueFromSources(null, [
+                                            [
+                                              store.activePlayerQueue?.repeat_mode == RepeatMode.OFF,
+                                              RepeatMode.ONE,
+                                            ],
+                                            [
+                                              store.activePlayerQueue?.repeat_mode == RepeatMode.ALL,
+                                              RepeatMode.OFF,
+                                            ],
+                                            [
+                                              store.activePlayerQueue?.repeat_mode == RepeatMode.ONE,
+                                              RepeatMode.ALL,
+                                            ],
+                                          ]),
+                                        )
+                                        else if (key === 'Digit1') router.push('/home');
+              else if (key === 'Digit2') router.push('/search');
+              else if (key === 'Digit3') router.push('/artists');
+              else if (key === 'Digit4')  router.push('/albums');
+              else if (key === 'Digit5') router.push('/tracks');
+              else if (key === 'Digit6') router.push('/playlists');
+              else if (key === 'Digit7')router.push('/radios');
+              else if (key === 'Digit8') router.push('/browse');
+              else { keyDetected = false; }
+          } else {
+            if (shiftPressed) {
+               
+               if (key === 'KeyS') router.push('/settings');
+               
+               else keyDetected = false; 
+            } else {
+              // else if (key === 'KeyR') // player side
+              if (key === 'ArrowLeft') api.queueCommandPrevious(store.activePlayerQueue!.queue_id);
+              else if (key === 'ArrowRight')  api.queueCommandNext(store.activePlayerQueue!.queue_id);
+              else keyDetected = false; 
+            }
+            
+          }
+      } else {
+        if (shiftPressed) {
+          if (key === 'Slash') store.showKeyHelper = (store.showKeyHelper ? false : true);
+          else keyDetected = false;
+        } else {
+          if (key === 'Space') api.queueCommandPlayPause(store.activePlayerQueue!.queue_id);
+          else keyDetected = false;
+        }
+      }
+      console.log(store.showKeyHelper);
+      if (keyDetected)
+        e.preventDefault();
+  }
+
+  window.addEventListener('keydown', handleKeyDown, true)
   // Initialize API Connection
   // TODO: retrieve serveraddress through discovery and/or user settings ?
   let serverAddress = '';
