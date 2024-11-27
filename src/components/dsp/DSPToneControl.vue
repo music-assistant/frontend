1db6d767bd39f52cc2f38f1a7f912fb8935dce74
Maxim Raznatovski <nda.mr43@gmail.com>
2024-11-27 14:01:33 +0100

    Feat: Add support for the ToneControl DSP Filter
    This is a simple 3 band equalizer that replaces the old CONF_EQ_BASS,
    CONF_EQ_MID and CONF_EQ_TREBLE options.

---
+++ src/components/dsp/DSPToneControl.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card-item>
+    <DSPSlider
+      v-model="tone_control.bass_level"
+      :type="{
+        min: -10,
+        max: 10,
+        step: 0.1,
+        label: $t('settings.dsp.tone_control.bass_level'),
+        unit: 'dB',
+      }"
+    />
+  </v-card-item>
+  <v-card-item>
+    <DSPSlider
+      v-model="tone_control.mid_level"
+      :type="{
+        min: -10,
+        max: 10,
+        step: 0.1,
+        label: $t('settings.dsp.tone_control.mid_level'),
+        unit: 'dB',
+      }"
+    />
+  </v-card-item>
+  <v-card-item>
+    <DSPSlider
+      v-model="tone_control.treble_level"
+      :type="{
+        min: -10,
+        max: 10,
+        step: 0.1,
+        label: $t('settings.dsp.tone_control.treble_level'),
+        unit: 'dB',
+      }"
+    />
+  </v-card-item>
+</template>
+<script setup lang="ts">
+import { ToneControlFilter } from "@/plugins/api/interfaces";
+import DSPSlider from "./DSPSlider.vue";
+
+const tone_control = defineModel<ToneControlFilter>({ required: true });
+</script>
