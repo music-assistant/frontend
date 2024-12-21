cfa43b8cdef1369a5de79fe6250fa951693c1fc8
JLCD <jean-luc@jlcd.de>
2024-12-21 11:46:03 +0100

    feat(BrowserMediaControl) small fixes

--- src/helpers/useMediaBrowserMetaData.ts
+++ src/helpers/useMediaBrowserMetaData.ts
@@ -1,20 +1,13 @@
-import api from "@/plugins/api";
-import {
-  MediaItem,
-  PlayerMedia,
-  PlayerState,
-  QueueItem,
-  Track,
-} from "@/plugins/api/interfaces";
+import { MediaType, QueueItem, Track } from "@/plugins/api/interfaces";
 import { store } from "@/plugins/store";
-import { onMounted, watch } from "vue";
+import { watch } from "vue";
 
 function playerMediaToMetadata(item: QueueItem) {
   let artist: string | undefined;
   let album: string | undefined;
 
   //here we cast to Track to access properties that are only available on Track
-  if (item.media_item!.media_type === "track") {
+  if (item.media_item!.media_type === MediaType.TRACK) {
     const currentMedia = item.media_item as Track;
     artist = currentMedia.artists.map((a) => a.name).join(", ");
     album = currentMedia.album.name;
@@ -29,18 +22,18 @@ function playerMediaToMetadata(item: QueueItem) {
 }
 
 export function useMediaBrowserMetaData() {
-  let currentMediaData: MediaMetadata | undefined;
+  let currentMediaUri: string | undefined;
 
   //watch the current media to update the metadata
   watch(
     () => store.curQueueItem,
     (newMedia) => {
       if (!newMedia || !newMedia.media_item) return;
-      const newMediaMetaData = playerMediaToMetadata(newMedia);
       //Lets make sure that the new media isnt spammed
-      if (currentMediaData?.title === newMediaMetaData.title) return;
-      currentMediaData = playerMediaToMetadata(newMedia);
-      navigator.mediaSession.metadata = currentMediaData;
+      if (newMedia.media_item.uri === currentMediaUri) return;
+      const newMediaMetaData = playerMediaToMetadata(newMedia);
+      currentMediaUri = newMedia.media_item.uri;
+      navigator.mediaSession.metadata = newMediaMetaData;
     },
   );
 }
