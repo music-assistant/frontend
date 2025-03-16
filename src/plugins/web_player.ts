9c7d3987c14ce9d803eaba599c00bd6dd9894e0c
Maxim Raznatovski <nda.mr43@gmail.com>
2025-03-16 19:26:52 +0100

    feat: ensure only one tab plays music

--- src/plugins/web_player.ts
+++ src/plugins/web_player.ts
@@ -10,6 +10,117 @@ export enum WebPlayerMode {
 
 let unsubSubscriptions: (() => void)[] = [];
 
+// We use a channel to communicate with all other tabs of MA open.
+// This allows us to limit playback to only a single tab.
+// Playback can still be controlled from any tab, this just avoids double playback
+// of the music, and is independant from the player implementation used by the frontend.
+// In case the "leading" tab is closed, another open tab will take controll.
+const bc = new BroadcastChannel("web-player");
+
+const BC_MSG = {
+  IS_ACTIVE: "IS_WEBPLAYER_ACTIVE",
+  IS_ACTIVE_RESPONSE: "WEBPLAYER_IS_ACTIVE",
+  TAKING_CONTROL: "TAKING_CONTROL:", // Followed by the priority
+  CONTROL_AVAILABLE: "CONTROL_AVAILABLE",
+};
+
+const uniqueId = crypto.randomUUID();
+
+bc.onmessage = (event) => {
+  if (
+    typeof event.data === "string" &&
+    event.data.startsWith(BC_MSG.TAKING_CONTROL)
+  ) {
+    // Another tab is taking control, silently switch back to just the notification
+    // (maybe this tab was suspended by the browser and didn't respond in time?)
+    if (webPlayer.tabMode === WebPlayerMode.BUILTIN) {
+      // TODO: don't unregister in this case
+      webPlayer.disable();
+    }
+    const priority = event.data.substring(BC_MSG.TAKING_CONTROL.length);
+    if (highestPriority !== undefined)
+      highestPriority = highestPriority > priority ? highestPriority : priority;
+    else highestPriority = priority;
+  }
+  switch (event.data) {
+    case BC_MSG.IS_ACTIVE:
+      // Respond if this tab is active
+      if (webPlayer.tabMode === WebPlayerMode.BUILTIN && webPlayer.player_id) {
+        bc.postMessage(BC_MSG.IS_ACTIVE_RESPONSE);
+      }
+      break;
+    case BC_MSG.IS_ACTIVE_RESPONSE:
+      // Resolve any pending active player checks
+      activePlayerChecks.forEach((check) => {
+        clearTimeout(check.timeout);
+        check.resolve(true);
+      });
+      activePlayerChecks = [];
+      break;
+    case BC_MSG.CONTROL_AVAILABLE:
+      // Another tab released control, take over if desired
+      if (
+        webPlayer.mode === WebPlayerMode.BUILTIN &&
+        webPlayer.tabMode !== WebPlayerMode.BUILTIN
+      ) {
+        webPlayer.setTabMode(WebPlayerMode.BUILTIN);
+      }
+      break;
+  }
+};
+
+// Called on close
+window.addEventListener("unload", function () {
+  if (webPlayer.tabMode === WebPlayerMode.BUILTIN && webPlayer.player_id) {
+    bc.postMessage(BC_MSG.CONTROL_AVAILABLE);
+  }
+});
+
+// Track active player checks
+interface ActiveCheck {
+  resolve: (value: boolean) => void;
+  timeout: number;
+}
+let activePlayerChecks: ActiveCheck[] = [];
+
+async function isAnotherTabActive(): Promise<boolean> {
+  return new Promise((resolve) => {
+    const timeout = window.setTimeout(() => {
+      activePlayerChecks = activePlayerChecks.filter(
+        (check) => check.timeout !== timeout,
+      );
+      resolve(false); // No response within 500ms, assume no active player
+    }, 500);
+
+    activePlayerChecks.push({ resolve, timeout });
+
+    // Send the message, onmessage will resolve to true in case another tab responds
+    bc.postMessage(BC_MSG.IS_ACTIVE);
+  });
+}
+
+let highestPriority: string | undefined;
+
+async function canTakeControl(): Promise<boolean> {
+  // Generate a unique priority string with interaction as a prefix
+  // (so interacted with tabs are prioritized)
+  const priority = (webPlayer.interacted ? "1" : "0") + uniqueId;
+
+  if (highestPriority !== undefined)
+    highestPriority = highestPriority > priority ? highestPriority : priority;
+  else highestPriority = priority;
+  bc.postMessage(BC_MSG.TAKING_CONTROL + priority);
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // Compare lexicographically - only one tab can win
+      const wonControl = highestPriority === priority;
+      highestPriority = undefined;
+      resolve(wonControl);
+    }, 2000);
+  });
+}
+
 export const webPlayer = reactive({
   mode: WebPlayerMode.DISABLED,
   tabMode: WebPlayerMode.DISABLED,
@@ -20,7 +131,6 @@ export const webPlayer = reactive({
   async setMode(mode: WebPlayerMode) {
     if (this.mode === mode) return;
     this.mode = mode;
-    // TODO: assure that only one tab can be played to.
     this.setTabMode(mode);
   },
   async setTabMode(mode: WebPlayerMode) {
@@ -33,11 +143,26 @@ export const webPlayer = reactive({
 
     if (this.tabMode === WebPlayerMode.BUILTIN) {
       if (this.player_id) {
+        bc.postMessage(BC_MSG.CONTROL_AVAILABLE); // Notify other tabs
         await api.unregisterBuiltinPlayer(this.player_id);
       }
     }
     this.audioSource = WebPlayerMode.DISABLED;
     this.player_id = null;
+
+    // If trying to set to a player mode, check if another tab already has it
+    if (mode === WebPlayerMode.BUILTIN) {
+      if (await isAnotherTabActive()) {
+        // Another tab already is already responsible for the playback, fall back to CONTROLS_ONLY
+        mode = WebPlayerMode.CONTROLS_ONLY;
+      } else {
+        // No other tab has control, but another tab tries to take control simultaneously?
+        if (!(await canTakeControl())) {
+          // No, another tab will take it from here
+          mode = WebPlayerMode.CONTROLS_ONLY;
+        }
+      }
+    }
 
     this.tabMode = mode;
 
