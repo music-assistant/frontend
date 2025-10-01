8c5f899410bff962202efe44011f8f593b56c903
stvncode <steven.travers20@gmail.com>
2025-10-01 15:48:24 +0200

    Update breadcrumb

--- src/views/settings/Settings.vue
+++ src/views/settings/Settings.vue
@@ -51,6 +51,7 @@
 
 <script setup lang="ts">
 import Toolbar from "@/components/Toolbar.vue";
+import { match } from "ts-pattern";
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRouter } from "vue-router";
@@ -74,7 +75,15 @@ const activeTab = computed(() => {
 });
 
 const breadcrumbItems = computed(() => {
-  const items = [
+  const route = router.currentRoute.value;
+  const name = route.name?.toString() || "";
+
+  const items: Array<{
+    title: string;
+    disabled: boolean;
+    href?: string;
+    to?: any;
+  }> = [
     {
       title: t("settings.settings"),
       disabled: false,
@@ -84,14 +93,60 @@ const breadcrumbItems = computed(() => {
   ];
 
   const currentTab = activeTab.value;
-  if (currentTab) {
+  if (currentTab === "players") {
     items.push({
-      title: t(`settings.${currentTab}`),
-      disabled: true,
-      href: "#",
+      title: t("settings.players"),
+      disabled: name === "playersettings",
+      to: { name: "playersettings" },
+    });
+  } else if (currentTab === "core") {
+    items.push({
+      title: t("settings.core"),
+      disabled: name === "coresettings",
+      to: { name: "coresettings" },
+    });
+  } else if (currentTab === "frontend") {
+    items.push({
+      title: t("settings.frontend"),
+      disabled: name === "frontendsettings",
+      to: { name: "frontendsettings" },
+    });
+  } else {
+    items.push({
+      title: t("settings.providers"),
+      disabled: name === "providersettings",
       to: { name: "providersettings" },
     });
   }
+
+  match(name)
+    .with("addprovider", () => {
+      items.push({ title: t("settings.add_provider"), disabled: true });
+    })
+    .with("addproviderdetails", () => {
+      items.push({
+        title: t("settings.setup_provider", [route.params.domain || ""]),
+        disabled: true,
+      });
+    })
+    .with("editprovider", () => {
+      items.push({
+        title: t("settings.edit_provider", [route.params.instanceId || ""]),
+        disabled: true,
+      });
+    })
+    .with("addgroup", () => {
+      items.push({ title: t("settings.add_group_player"), disabled: true });
+    })
+    .with("editplayer", () => {
+      items.push({ title: t("settings.player_settings"), disabled: true });
+    })
+    .with("editcore", () => {
+      items.push({ title: t("settings.settings"), disabled: true });
+    })
+    .otherwise(() => {
+      return;
+    });
 
   return items;
 });
