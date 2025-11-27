165a4cf1ee33f701e0acf9e32c389fdb5dd8d095
Steven Travers <steven.travers20@gmail.com>
2025-11-28 00:41:19 +0100

    Implement new toaster and remove the old one (#1229)

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -7,17 +7,6 @@
       </router-view>
       <add-to-playlist-dialog />
       <item-context-menu />
-      <v-snackbar
-        :model-value="store.activeAlert !== undefined"
-        :color="store.activeAlert?.type || 'grey'"
-        :text="store.activeAlert?.message"
-        close-on-content-click
-        position="sticky"
-        :timeout="store.activeAlert?.persistent ? -1 : 5000"
-        z-index="999999"
-        style="z-index: 999999"
-        @update:model-value="() => (store.activeAlert = undefined)"
-      />
     </div>
   </v-main>
 </template>
