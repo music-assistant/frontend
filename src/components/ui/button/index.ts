b7b7d8fa21def97e196dc509fdf0ae63256e4373
stvncode <steven.travers20@gmail.com>
2025-12-15 17:49:39 +0100

    Change icons for player

--- src/components/ui/button/index.ts
+++ src/components/ui/button/index.ts
@@ -16,6 +16,7 @@ export const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        "ghost-icon": "hover:bg-transparent",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
