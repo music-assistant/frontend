2e2c7289985d4e2e9cf8bbe24b6ffb77217d445a
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-02-10 20:02:36 +0100

    player options + interfaces

--- src/plugins/api/interfaces.ts
+++ src/plugins/api/interfaces.ts
@@ -653,7 +653,7 @@ export interface ItemMapping extends _MediaItemBase {
   year?: number;
 }
 
-export interface Artist extends MediaItem { }
+export interface Artist extends MediaItem {}
 
 export interface Album extends MediaItem {
   year?: number;
@@ -675,7 +675,7 @@ export interface Playlist extends MediaItem {
   is_editable: boolean;
 }
 
-export interface Radio extends MediaItem { }
+export interface Radio extends MediaItem {}
 
 export interface Audiobook extends MediaItem {
   publisher: string;
