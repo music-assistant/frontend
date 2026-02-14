c9f8267378c3d08ba0e157edea5c7a013174a357
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2026-02-14 02:38:21 +0100

    separate PlayerOptionField

--- src/views/settings/EditPlayerOptions.vue
+++ src/views/settings/EditPlayerOptions.vue
@@ -1,33 +1,9 @@
 <template>
   <section class="edit-player-options"></section>
-  <div v-for="option in playerOptionsSwitch" :key="option.key">
-    {{ option.key }}
-    {{ option.value }}
-    <v-switch
-      :model-value="option.value"
-      color="primary"
-      hide-details
-      class="status-switch"
-      @update:model-value="(value) => uiSetPlayerOption(option.key, value)"
-    />
-  </div>
-  <div v-for="option in playerOptionsNumber" :key="option.key">
-    <div v-if="typeof option.value === 'number'">
-      {{ option.key }}
-      {{ option.value }}
-      <Slider
-        :model-value="[option.value]"
-        class="w-56"
-        :step="option.step"
-        :min="option.min_value"
-        :max="option.max_value"
-        @update:model-value="(value) => uiSetPlayerOption(option.key, value)"
-      />
+  <div v-if="props.playerId">
+    <div v-for="option in playerOptions" :key="option.key">
+      <PlayerOptionField :player-option="option" :player-id="props.playerId" />
     </div>
-  </div>
-  <div v-for="option in playerOptionsSelect" :key="option.key">
-    {{ option.key }}
-    {{ option.value }}
   </div>
 </template>
 
@@ -41,6 +17,7 @@ import {
 } from "@/plugins/api/interfaces";
 import { onBeforeUnmount, computed, ref, Ref } from "vue";
 import Slider from "@/components/ui/slider/Slider.vue";
+import PlayerOptionField from "./PlayerOptionField.vue";
 
 // props
 const props = defineProps<{
@@ -49,75 +26,6 @@ const props = defineProps<{
 
 // global refs
 const playerOptions = ref<PlayerOption[]>([]);
-
-// computed properties
-// helper function
-const getOptions = (
-  type: PlayerOptionType[],
-  readOnly: boolean,
-  hasOptions: boolean = false,
-) => {
-  let array: PlayerOption[] = [];
-  playerOptions.value.forEach((option) => {
-    if (option.options && option.options.length > 0) {
-      if (
-        hasOptions &&
-        option.read_only === readOnly &&
-        type.includes(option.type)
-      ) {
-        // has suboptions
-        array.push(option);
-      }
-    } else if (
-      !hasOptions &&
-      option.read_only === readOnly &&
-      type.includes(option.type)
-    ) {
-      // no sub options
-      array.push(option);
-    }
-  });
-  // sort them to make sure, that we always have the options in the same order.
-  array = array.sort((a, b) => b.key.localeCompare(a.key));
-  return array;
-};
-
-const playerOptionsSwitch = computed(() => {
-  return getOptions([PlayerOptionType.BOOLEAN], false);
-});
-
-const playerOptionsNumber = computed(() => {
-  return getOptions([PlayerOptionType.INTEGER, PlayerOptionType.FLOAT], false);
-});
-
-const playerOptionsText = computed(() => {
-  return getOptions([PlayerOptionType.STRING], false);
-});
-
-const playerOptionsSelect = computed(() => {
-  return getOptions(
-    [
-      PlayerOptionType.BOOLEAN,
-      PlayerOptionType.INTEGER,
-      PlayerOptionType.FLOAT,
-      PlayerOptionType.STRING,
-    ],
-    false,
-    true,
-  );
-});
-
-const playerOptionsSensor = computed(() => {
-  return getOptions(
-    [
-      PlayerOptionType.BOOLEAN,
-      PlayerOptionType.INTEGER,
-      PlayerOptionType.FLOAT,
-      PlayerOptionType.STRING,
-    ],
-    true,
-  );
-});
 
 // Full load on entry
 const loadOptionsOnEntry = async () => {
@@ -147,20 +55,4 @@ const unsub = api.subscribe(
   props.playerId,
 );
 onBeforeUnmount(unsub);
-
-// ui methods
-
-const uiSetPlayerOption = async (
-  key: string,
-  value: PlayerOptionValueType | PlayerOptionValueType[] | null | undefined,
-) => {
-  if (!props.playerId || value === null || value == undefined) return;
-  // we only have single numbers
-  if (Array.isArray(value)) value = value[0];
-  try {
-    await api.playerCommandSetOption(props.playerId, key, value);
-  } catch (error) {
-    console.error("Error toggling option:", key);
-  }
-};
 </script>
---
+++ src/views/settings/PlayerOptionField.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="player-option-field"></div>
+
+  <!-- read only, simply show value -->
+  <div v-if="playerOption.read_only">
+    <v-label>{{ getTranslatedLabel() }} </v-label>
+    <v-label>{{ playerOption.value }} </v-label>
+  </div>
+
+  <!-- toggle for boolean values -->
+  <div
+    v-else-if="playerOption.type == PlayerOptionType.BOOLEAN"
+    class="po-toggle"
+  >
+    <v-label>{{ getTranslatedLabel() }} </v-label>
+    <v-switch
+      :model-value="playerOption.value"
+      @update:model-value="
+        (value) => uiSetPlayerOption(playerOption.key, value)
+      "
+    />
+  </div>
+
+  <!-- slider for int/ float with min/max -->
+  <div
+    v-else-if="
+      (playerOption.type == PlayerOptionType.INTEGER ||
+        playerOption.type == PlayerOptionType.FLOAT) &&
+      playerOption.min_value &&
+      playerOption.max_value &&
+      playerOption.step
+    "
+  >
+    <v-label>{{ getTranslatedLabel() }} </v-label>
+    <div>
+      <v-slider
+        :model-value="playerOption.value as number"
+        :min="playerOption.min_value"
+        :max="playerOption.max_value"
+        :step="playerOption.step"
+        show-ticks="always"
+        @update:model-value="
+          (value) => uiSetPlayerOption(playerOption.key, value)
+        "
+      >
+        <template #append>
+          <v-text-field
+            v-model="playerOption.value as number"
+            density="compact"
+            style="width: 80px"
+            type="number"
+            variant="outlined"
+            hide-details
+          />
+        </template>
+      </v-slider>
+    </div>
+  </div>
+
+  <!-- text field for int/ float without a range -->
+  <v-text-field
+    v-else-if="
+      playerOption.type == PlayerOptionType.INTEGER ||
+      playerOption.type == PlayerOptionType.FLOAT
+    "
+    :model-value="playerOption.value"
+    :label="getTranslatedLabel()"
+    :clearable="false"
+    type="number"
+    variant="outlined"
+    density="comfortable"
+    @update:model-value="(value) => uiSetPlayerOption(playerOption.key, value)"
+  />
+
+  <!-- text field for string -->
+  <v-text-field
+    v-else-if="playerOption.type == PlayerOptionType.STRING"
+    :model-value="playerOption.value"
+    :label="getTranslatedLabel()"
+    :clearable="false"
+    type="string"
+    variant="outlined"
+    density="comfortable"
+    @update:model-value="(value) => uiSetPlayerOption(playerOption.key, value)"
+  />
+
+  <!-- drop down for multiple options -->
+  <v-select
+    v-else-if="playerOption.options && playerOption.options.length > 0"
+    :model-value="playerOption.value"
+    :items="translatedOptions"
+    :label="getTranslatedLabel()"
+    variant="outlined"
+    @update:model-value="(value) => uiSetPlayerOption(playerOption.key, value)"
+  />
+</template>
+
+<script setup lang="ts">
+import api from "@/plugins/api";
+import {
+  PlayerOption,
+  PlayerOptionEntry,
+  PlayerOptionValueType,
+} from "@/plugins/api/interfaces";
+import { PlayerOptionType } from "@/plugins/api/interfaces";
+import { $t } from "@/plugins/i18n";
+import { computed } from "vue";
+
+const props = defineProps<{
+  playerOption: PlayerOption;
+  playerId: string;
+}>();
+
+const uiSetPlayerOption = async (
+  key: string,
+  value: PlayerOptionValueType | PlayerOptionValueType[] | null | undefined,
+) => {
+  if (!props.playerId || value === null || value == undefined) return;
+  // we only have single numbers
+  if (Array.isArray(value)) value = value[0];
+  try {
+    await api.playerCommandSetOption(props.playerId, key, value);
+  } catch (error) {
+    console.error("Error toggling option:", key);
+  }
+};
+
+// Taken from ConfigEntryField
+const getTranslatedLabel = () => {
+  // prefer translation_key over key (using key for translations is deprecated)
+  const key = props.playerOption.translation_key || props.playerOption.key;
+  const translationKey = `player_options.${key}`;
+  const fallback = props.playerOption.name;
+
+  // If translation_params are provided, pass them directly
+  if (
+    props.playerOption.translation_params &&
+    props.playerOption.translation_params.length > 0
+  ) {
+    return (
+      $t(translationKey, props.playerOption.translation_params) || fallback
+    );
+  }
+
+  return $t(translationKey, fallback);
+};
+
+const translatedOptions = computed(() => {
+  // it needs to have title and value for the dropdown
+  interface translatedOption {
+    title: string;
+    value: PlayerOptionValueType;
+  }
+  if (!props.playerOption.options) return [];
+  const options: translatedOption[] = [];
+  for (const orgOption of props.playerOption.options) {
+    let cleanKey = orgOption.key.toString() || "";
+    let cleanName = orgOption.name.toString() || "";
+    for (const specialChar of ["@", "$", "|"]) {
+      if (cleanKey.includes(specialChar)) {
+        cleanKey = cleanKey.toString().replaceAll(specialChar, "");
+      }
+      if (cleanName.includes(specialChar)) {
+        cleanName = cleanName.toString().replaceAll(specialChar, "");
+      }
+    }
+    let optName = $t(
+      `player_options.${props.playerOption.key}.options.${cleanKey}`,
+      cleanName,
+    );
+    const option = {
+      // key and value are the same, should models change?
+      title: optName,
+      value: orgOption.value,
+    };
+    options.push(option);
+  }
+  return options;
+});
+</script>
