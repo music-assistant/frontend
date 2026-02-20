6826ff08880ff1ed3c64c42ce7d1f2c7cc3d1a19
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-02-19 22:23:08 -0800

    Commenting out test code

--- test/components/InfoHeader.test.ts
+++ test/components/InfoHeader.test.ts
@@ -1,7 +1,40 @@
 import { describe, expect, it } from "vitest";
-import InfoHeader from "@/components/InfoHeader.vue";
+import { mount } from "@vue/test-utils";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-it("Example Test", () => {
-  expect(1 + 1 == 2).toBe(true);
+import InfoHeader from "@/components/InfoHeader.vue";
+import { MediaItemType, Playlist } from "@/plugins/api/interfaces";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+// global.ResizeObserver = require("resize-observer-polyfill");
+
+const playlist: Playlist = {
+  owner: "Playlist Owner",
+  is_editable: false,
+} as Playlist;
+
+it("displays message", () => {
+  const wrapper = mount(
+    {
+      template: "<v-layout><InfoHeader/></v-layout>",
+    },
+    {
+      props: {
+        item: playlist,
+      },
+      global: {
+        components: {
+          InfoHeader,
+        },
+        plugins: [vuetify],
+      },
+    },
+  );
   return;
 });
