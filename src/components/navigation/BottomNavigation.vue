658ce0d726173e79c8d3cd9babaa12f23b53057a
Marcel van der Veldt <m.vanderveldt@outlook.com>
2026-03-01 17:06:30 +0100

    Highlight active page

--- src/components/navigation/BottomNavigation.vue
+++ src/components/navigation/BottomNavigation.vue
@@ -20,21 +20,32 @@
       :aria-label="$t('discover')"
       tabindex="0"
       variant="text"
+      :active="isActive('discover')"
       active-color="fg"
       @click="handleDiscoverClick"
     >
-      <Home class="w-5 h-5" />
-      <span class="menuButton">{{ $t("discover") }}</span>
+      <Home class="w-5 h-5" :stroke-width="isActive('discover') ? 2.5 : 2" />
+      <span
+        class="menuButton"
+        :class="{ 'menuButton--active': isActive('discover') }"
+        >{{ $t("discover") }}</span
+      >
     </v-btn>
 
     <v-btn
       :aria-label="$t('search')"
       tabindex="0"
       variant="text"
+      :active="isActive('search')"
+      active-color="fg"
       @click="handleSearchClick"
     >
-      <Search class="w-5 h-5" />
-      <span class="menuButton">{{ $t("search") }}</span>
+      <Search class="w-5 h-5" :stroke-width="isActive('search') ? 2.5 : 2" />
+      <span
+        class="menuButton"
+        :class="{ 'menuButton--active': isActive('search') }"
+        >{{ $t("search") }}</span
+      >
     </v-btn>
 
     <ActivePlayerPopover auto-show align="end" @click="handlePlayersClick">
@@ -57,10 +68,13 @@
 import { eventbus } from "@/plugins/eventbus";
 import { store } from "@/plugins/store";
 import { Home, Menu, Search, Speaker } from "lucide-vue-next";
-import { useRouter } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import ActivePlayerPopover from "@/components/ActivePlayerPopover.vue";
 
 const router = useRouter();
+const route = useRoute();
+
+const isActive = (name: string) => route.name === name;
 
 const handleMenuClick = () => {
   eventbus.emit("mobile-sidebar-open");
@@ -86,6 +100,10 @@ const handlePlayersClick = () => {
   font-stretch: condensed;
   text-transform: none;
   margin-top: 5px;
+}
+
+.menuButton--active {
+  font-weight: 600;
 }
 
 .v-btn--active > .v-btn__overlay {
