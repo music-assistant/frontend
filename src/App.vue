51f51a81981714eca832096661f2c9949f89d3db
Gav <gavnosp@hotmail.com>
2026-03-07 16:47:12 +1000

    Fix typing error

--- src/App.vue
+++ src/App.vue
@@ -388,8 +388,10 @@ onMounted(async () => {
     (newUser) => {
       if (newUser) {
         setTheme();
-        const userLangPref =
-          (newUser.preferences?.language as string) || "auto";
+        const langPref =
+          (store.currentUser?.preferences?.language as string | undefined) ||
+          localStorage.getItem("frontend.settings.language") ||
+          "auto";
         if (userLangPref !== "auto") {
           i18n.global.locale.value = userLangPref;
         }
