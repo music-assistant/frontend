dfc6bcdf13e4c0fa56cc8778087b9c76443d12a3
apophisnow <karlmohler@gmail.com>
2026-03-13 23:48:05 -0700

    fix: remove unneeded code for handling footer and nav

--- src/components/ui/sidebar/SidebarFooter.vue
+++ src/components/ui/sidebar/SidebarFooter.vue
@@ -1,36 +1,15 @@
 <script setup lang="ts">
-import { usePartyConfig } from "@/composables/usePartyConfig";
 import { cn } from "@/lib/utils";
 import type { HTMLAttributes } from "vue";
-import { computed, watch } from "vue";
-import { useRoute } from "vue-router";
+import { computed } from "vue";
 import { useSidebar } from "./utils";
 
 const props = defineProps<{
   class?: HTMLAttributes["class"];
 }>();
 
-const route = useRoute();
 const { isMobile, state } = useSidebar();
 const isCollapsed = computed(() => state.value === "collapsed");
-
-const { config: partyConfig, fetchConfig } = usePartyConfig();
-
-watch(
-  () => route.path,
-  async (path) => {
-    if (path === "/party") {
-      await fetchConfig();
-    }
-  },
-  { immediate: true },
-);
-
-const needsBottomPadding = computed(() => {
-  if (route.path !== "/party") return true;
-  if (partyConfig.value === null) return true;
-  return partyConfig.value?.show_player_controls ?? false;
-});
 </script>
 
 <template>
@@ -43,12 +22,8 @@ const needsBottomPadding = computed(() => {
         isMobile
           ? 'p-2'
           : isCollapsed
-            ? needsBottomPadding
-              ? 'pl-[1px] pt-2 pb-26'
-              : 'pl-[1px] pt-2'
-            : needsBottomPadding
-              ? 'px-3 pt-2 pb-26'
-              : 'px-3 pt-2',
+            ? 'pl-[1px] pt-2 pb-26'
+            : 'px-3 pt-2 pb-26',
         props.class,
       )
     "
