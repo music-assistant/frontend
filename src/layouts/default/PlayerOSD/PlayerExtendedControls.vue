3a7abaf9b85616497dd3384a925ae4305559d9eb
Moos, Daniel <daniel.moos@juliusbaer.com>
2026-04-04 21:45:21 +0200

    Move ⋯ menu before SpeakerBtn; simplify QueueBtn padding

--- src/layouts/default/PlayerOSD/PlayerExtendedControls.vue
+++ src/layouts/default/PlayerOSD/PlayerExtendedControls.vue
@@ -5,18 +5,15 @@
     align="end"
     child-element-id="extended-controls-speaker-button"
   />
+  <PlayerTrackMenu />
 
   <SpeakerBtn id="extended-controls-speaker-button" :color="player.color" />
 
   <QueueBtn
     v-if="queue && queue.isVisible"
     :color="queue.color"
-    :style="{
-      'padding-left': '15px',
-      'padding-right': isTrackMenuVisible ? '0px' : '20px',
-    }"
+    style="padding-left: 15px; padding-right: 20px"
   />
-  <PlayerTrackMenu />
   <PlayerVolume
     v-if="volume && volume.isVisible && store.activePlayer"
     :player="store.activePlayer"
@@ -28,17 +25,11 @@
 
 <script setup lang="ts">
 import ActivePlayerPopover from "@/components/ActivePlayerPopover.vue";
-import { MediaType } from "@/plugins/api/interfaces";
 import { store } from "@/plugins/store";
-import { computed } from "vue";
 import PlayerTrackMenu from "./PlayerControlBtn/PlayerTrackMenu.vue";
 import QueueBtn from "./PlayerControlBtn/QueueBtn.vue";
 import SpeakerBtn from "./PlayerControlBtn/SpeakerBtn.vue";
 import PlayerVolume from "./PlayerVolume.vue";
-
-const isTrackMenuVisible = computed(
-  () => store.curQueueItem?.media_item?.media_type === MediaType.TRACK,
-);
 
 // properties
 export interface Props {
