e87041f4594278fa0b4eb8934ed46dc8006fff28
stvncode <steven.travers20@gmail.com>
2026-06-24 23:52:36 +0200

    Update context menu with shadcn comp and lucide icons

--- src/layouts/default/PlayerOSD/PlayerFullscreen.vue
+++ src/layouts/default/PlayerOSD/PlayerFullscreen.vue
@@ -1239,6 +1239,11 @@ watchEffect(() => {
   const topColor = bgColor.lighten(0.25);
   const bottomColor = bgColor.darken(0.25);
   backgroundColor.value = `linear-gradient(to bottom, ${topColor.hex()}, ${bottomColor.hex()})`;
+
+  document.documentElement.style.setProperty(
+    "--fullscreen-player-bg",
+    backgroundColor.value,
+  );
 });
 </script>
 
