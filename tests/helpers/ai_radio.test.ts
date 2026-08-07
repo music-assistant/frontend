swiftbird07/ma-frontend

Add import / export functionality to AI-Radio. (https://github.com/orgs/music-assistant/discussions/5937)

tests/helpers/ai_radio.test.ts
@@ -1,4 +1,5 @@
 import {
+  buildSharedShow,
   buildStationPayload,
   defaultGuidedPlacement,
   errorMessage,
@@ -9,10 +10,15 @@ import {
   parseOptionalCapInput,
   parseOptionalNumber,
   parseOptionalPositiveInt,
+  parseSharedShow,
   playlistSelectValue,
   relativeTimeFromIso,
   safeInteger,
   safeNumber,
+  sharedShowFileName,
+  sharedShowToDraft,
+  sharedShowToJson,
+  type ShowDraft,
   slugify,
   splitPlaylistSelectValue,
   validateStationDraftLocal,
@@ -263,5 +269,243 @@ describe("relativeTimeFromIso", () => {
     expect(at("2026-07-16T11:45:00Z")).toBe("15m ago");
     expect(at("2026-07-16T10:00:00Z")).toBe("2h ago");
     expect(at("2026-07-13T12:00:00Z")).toBe("3d ago");
+  });
+});
+
+describe("share/import", () => {
+  const makeDraft = (): ShowDraft => ({
+    basics: {
+      id: "late_night",
+      name: "Late Night",
+      sourcePlaylistId: "42",
+      sourcePlaylistProvider: "library",
+      targetPlaylistProvider: "builtin",
+      defaultPlayerId: "player_1",
+      maxDurationMinutes: 90,
+      dynamicBatchSize: 4,
+      dynamicPollSeconds: 7,
+      dynamicPrefetchRemainingTracks: 3,
+      clearQueueOnStart: false,
+      general: {
+        instructions: "Host personality: calm.",
+        weather_provider: "open_meteo",
+        weather_timeout_seconds: 8,
+      },
+    },
+    segments: [
+      {
+        id: "intro",
+        name: "Intro",
+        prompt: "Open the show with <next_songinfo>.",
+        webSearch: "disabled",
+        maxChars: 650,
+        plays: { kind: "start" },
+      },
+      {
+        id: "fact",
+        name: "Artist fact",
+        prompt: "Share one fact.",
+        webSearch: "allow",
+        maxChars: 500,
+        plays: { kind: "every_n_songs", n: 2 },
+      },
+      {
+        id: "weather",
+        name: "Weather",
+        prompt: "Use <weather_hourly>.",
+        webSearch: "force",
+        maxChars: 400,
+        plays: { kind: "every_n_min", n: 60 },
+      },
+      {
+        id: "sign_off",
+        name: "Sign-off",
+        prompt: "Say goodbye.",
+        webSearch: "disabled",
+        maxChars: 300,
+        plays: { kind: "occasionally", percent: 25 },
+      },
+    ],
+  });
+
+  const roundTrip = (draft: ShowDraft) =>
+    parseSharedShow(sharedShowToJson(buildSharedShow(draft)));
+
+  it("round-trips persona and every segment field", () => {
+    const draft = makeDraft();
+    const imported = sharedShowToDraft(roundTrip(draft), {
+      itemId: "99",
+      provider: "spotify",
+    });
+
+    expect(imported.basics.general.instructions).toBe(
+      "Host personality: calm.",
+    );
+    expect(imported.segments.map((s) => s.name)).toEqual([
+      "Intro",
+      "Artist fact",
+      "Weather",
+      "Sign-off",
+    ]);
+    expect(imported.segments.map((s) => s.webSearch)).toEqual([
+      "disabled",
+      "allow",
+      "force",
+      "disabled",
+    ]);
+    expect(imported.segments.map((s) => s.maxChars)).toEqual([
+      650, 500, 400, 300,
+    ]);
+    expect(imported.segments.map((s) => s.plays)).toEqual(
+      draft.segments.map((s) => s.plays),
+    );
+  });
+
+  it("leaves the playlist, player and station id behind", () => {
+    const shared = buildSharedShow(makeDraft());
+    expect(JSON.stringify(shared)).not.toContain("player_1");
+    expect(Object.keys(shared)).toEqual([
+      "kind",
+      "version",
+      "name",
+      "instructions",
+      "segments",
+    ]);
+
+    const imported = sharedShowToDraft(shared);
+    expect(imported.basics.sourcePlaylistId).toBe("");
+    expect(imported.basics.sourcePlaylistProvider).toBe("library");
+    expect(imported.basics.defaultPlayerId).toBe("");
+    expect(imported.basics.id).toBeUndefined();
+  });
+
+  it("takes the importer's playlist choice", () => {
+    const imported = sharedShowToDraft(roundTrip(makeDraft()), {
+      itemId: "99",
+      provider: "spotify",
+    });
+    expect(imported.basics.sourcePlaylistId).toBe("99");
+    expect(imported.basics.sourcePlaylistProvider).toBe("spotify");
+  });
+
+  it("drops unknown keys instead of carrying them into the draft", () => {
+    // written as raw JSON so "__proto__" survives as a real key — an object
+    // literal would set the prototype and JSON.stringify would drop it
+    const shared = parseSharedShow(`{
+      "kind": "ai_radio_show",
+      "version": 1,
+      "name": "Sneaky",
+      "instructions": "",
+      "malicious": "payload",
+      "__proto__": { "polluted": true },
+      "segments": [
+        { "name": "Intro", "prompt": "Hi", "plays": { "kind": "start" },
+          "extra": "nope" }
+      ]
+    }`);
+
+    expect(shared).not.toHaveProperty("malicious");
+    expect(shared.segments[0]).not.toHaveProperty("extra");
+    expect(({} as Record<string, unknown>).polluted).toBeUndefined();
+
+    const imported = sharedShowToDraft(shared);
+    expect(Object.keys(imported.segments[0]).sort()).toEqual([
+      "id",
+      "maxChars",
+      "name",
+      "plays",
+      "prompt",
+      "webSearch",
+    ]);
+  });
+
+  it("gives colliding segment names unique ids", () => {
+    const shared = parseSharedShow(
+      JSON.stringify({
+        kind: "ai_radio_show",
+        version: 1,
+        name: "Twins",
+        segments: [
+          { name: "Break", prompt: "One", plays: { kind: "start" } },
+          { name: "Break", prompt: "Two", plays: { kind: "end" } },
+        ],
+      }),
+    );
+    const ids = sharedShowToDraft(shared).segments.map((s) => s.id);
+    expect(ids).toEqual(["break", "break_2"]);
+  });
+
+  it("falls back to disabled for an unknown web search mode", () => {
+    const shared = parseSharedShow(
+      JSON.stringify({
+        kind: "ai_radio_show",
+        version: 1,
+        name: "Odd",
+        segments: [
+          {
+            name: "Intro",
+            prompt: "Hi",
+            webSearch: "always",
+            plays: { kind: "start" },
+          },
+        ],
+      }),
+    );
+    expect(shared.segments[0].webSearch).toBe("disabled");
+  });
+
+  it("rejects documents that are not a valid shared show", () => {
+    const invalid = "providers.ai_radio.validation.invalid_import_file";
+    const base = {
+      kind: "ai_radio_show",
+      version: 1,
+      name: "Show",
+      segments: [{ name: "Intro", prompt: "Hi", plays: { kind: "start" } }],
+    };
+    const rejects = [
+      "not json at all",
+      "[]",
+      JSON.stringify({ ...base, kind: "something_else" }),
+      JSON.stringify({ ...base, version: 2 }),
+      JSON.stringify({ ...base, name: "   " }),
+      JSON.stringify({ ...base, segments: [] }),
+      JSON.stringify({
+        ...base,
+        segments: [{ name: "Intro", prompt: "", plays: { kind: "start" } }],
+      }),
+      JSON.stringify({
+        ...base,
+        segments: [
+          { name: "Intro", prompt: { evil: true }, plays: { kind: "start" } },
+        ],
+      }),
+      JSON.stringify({
+        ...base,
+        segments: [{ name: "Intro", prompt: "Hi", plays: { kind: "never" } }],
+      }),
+      JSON.stringify({
+        ...base,
+        segments: [{ name: "Intro", prompt: "x".repeat(8001) }],
+      }),
+      JSON.stringify({
+        ...base,
+        segments: Array.from({ length: 51 }, () => ({
+          name: "Intro",
+          prompt: "Hi",
+          plays: { kind: "start" },
+        })),
+      }),
+    ];
+    for (const payload of rejects) {
+      expect(() => parseSharedShow(payload)).toThrowError(invalid);
+    }
+  });
+});
+
+describe("sharedShowFileName", () => {
+  it("slugifies the show name", () => {
+    expect(sharedShowFileName("Late Night Deep Cuts")).toBe(
+      "late_night_deep_cuts.ai-radio-show.json",
+    );
   });
 });

music-assistant/frontend

tests/helpers/ai_radio.test.ts
@@ -1,9 +1,14 @@
 import {
   asGeneralDefaults,
+  buildSharedShow,
   compileShow,
   decompileStation,
   errorMessage,
+  parseSharedShow,
   relativeTimeFromIso,
+  sharedShowFileName,
+  sharedShowToDraft,
+  sharedShowToJson,
   slugify,
 } from "@/helpers/ai_radio";
 import type { ShowDraft } from "@/helpers/ai_radio";
@@ -109,5 +114,239 @@ describe("decompileStation", () => {
   it("defaults shuffleSourceTracks to true when absent from the station", () => {
     const { basics } = decompileStation(makeStation(), [makeSection()]);
     expect(basics.shuffleSourceTracks).toBe(true);
+  });
+});
+
+describe("share/import", () => {
+  const makeShareDraft = (): ShowDraft => ({
+    basics: {
+      id: "late_night",
+      name: "Late Night",
+      sourcePlaylistId: "42",
+      sourcePlaylistProvider: "library",
+      defaultPlayerId: "player_1",
+      maxDurationMinutes: 90,
+      shuffleSourceTracks: false,
+      general: {
+        instructions: "Host personality: calm.",
+        weather_provider: "open_meteo",
+        weather_timeout_seconds: 8,
+      },
+    },
+    segments: [
+      {
+        id: "intro",
+        name: "Intro",
+        prompt: "Open the show with <next_songinfo>.",
+        webSearch: "disabled",
+        maxChars: 650,
+        plays: { kind: "start" },
+      },
+      {
+        id: "fact",
+        name: "Artist fact",
+        prompt: "Share one fact.",
+        webSearch: "allow",
+        maxChars: 500,
+        plays: { kind: "every_n_songs", n: 2 },
+      },
+      {
+        id: "weather",
+        name: "Weather",
+        prompt: "Use <weather_hourly>.",
+        webSearch: "force",
+        maxChars: 400,
+        plays: { kind: "every_n_min", n: 60 },
+      },
+      {
+        id: "sign_off",
+        name: "Sign-off",
+        prompt: "Say goodbye.",
+        webSearch: "disabled",
+        maxChars: 300,
+        plays: { kind: "occasionally", percent: 25 },
+      },
+    ],
+  });
+
+  const roundTrip = (draft: ShowDraft) =>
+    parseSharedShow(sharedShowToJson(buildSharedShow(draft)));
+
+  it("round-trips persona and every segment field", () => {
+    const draft = makeShareDraft();
+    const imported = sharedShowToDraft(roundTrip(draft), {
+      itemId: "99",
+      provider: "spotify",
+    });
+
+    expect(imported.basics.general.instructions).toBe(
+      "Host personality: calm.",
+    );
+    expect(imported.segments.map((s) => s.name)).toEqual([
+      "Intro",
+      "Artist fact",
+      "Weather",
+      "Sign-off",
+    ]);
+    expect(imported.segments.map((s) => s.webSearch)).toEqual([
+      "disabled",
+      "allow",
+      "force",
+      "disabled",
+    ]);
+    expect(imported.segments.map((s) => s.maxChars)).toEqual([
+      650, 500, 400, 300,
+    ]);
+    expect(imported.segments.map((s) => s.plays)).toEqual(
+      draft.segments.map((s) => s.plays),
+    );
+  });
+
+  it("leaves the playlist, player and station id behind", () => {
+    const shared = buildSharedShow(makeShareDraft());
+    expect(JSON.stringify(shared)).not.toContain("player_1");
+    expect(Object.keys(shared)).toEqual([
+      "kind",
+      "version",
+      "name",
+      "instructions",
+      "segments",
+    ]);
+
+    const imported = sharedShowToDraft(shared);
+    expect(imported.basics.sourcePlaylistId).toBe("");
+    expect(imported.basics.sourcePlaylistProvider).toBe("library");
+    expect(imported.basics.defaultPlayerId).toBe("");
+    expect(imported.basics.id).toBeUndefined();
+  });
+
+  it("takes the importer's playlist choice", () => {
+    const imported = sharedShowToDraft(roundTrip(makeShareDraft()), {
+      itemId: "99",
+      provider: "spotify",
+    });
+    expect(imported.basics.sourcePlaylistId).toBe("99");
+    expect(imported.basics.sourcePlaylistProvider).toBe("spotify");
+  });
+
+  it("drops unknown keys instead of carrying them into the draft", () => {
+    // written as raw JSON so "__proto__" survives as a real key — an object
+    // literal would set the prototype and JSON.stringify would drop it
+    const shared = parseSharedShow(`{
+      "kind": "ai_radio_show",
+      "version": 1,
+      "name": "Sneaky",
+      "instructions": "",
+      "malicious": "payload",
+      "__proto__": { "polluted": true },
+      "segments": [
+        { "name": "Intro", "prompt": "Hi", "plays": { "kind": "start" },
+          "extra": "nope" }
+      ]
+    }`);
+
+    expect(shared).not.toHaveProperty("malicious");
+    expect(shared.segments[0]).not.toHaveProperty("extra");
+    expect(({} as Record<string, unknown>).polluted).toBeUndefined();
+
+    const imported = sharedShowToDraft(shared);
+    expect(Object.keys(imported.segments[0]).sort()).toEqual([
+      "id",
+      "maxChars",
+      "name",
+      "plays",
+      "prompt",
+      "webSearch",
+    ]);
+  });
+
+  it("gives colliding segment names unique ids", () => {
+    const shared = parseSharedShow(
+      JSON.stringify({
+        kind: "ai_radio_show",
+        version: 1,
+        name: "Twins",
+        segments: [
+          { name: "Break", prompt: "One", plays: { kind: "start" } },
+          { name: "Break", prompt: "Two", plays: { kind: "end" } },
+        ],
+      }),
+    );
+    const ids = sharedShowToDraft(shared).segments.map((s) => s.id);
+    expect(ids).toEqual(["break", "break_2"]);
+  });
+
+  it("falls back to disabled for an unknown web search mode", () => {
+    const shared = parseSharedShow(
+      JSON.stringify({
+        kind: "ai_radio_show",
+        version: 1,
+        name: "Odd",
+        segments: [
+          {
+            name: "Intro",
+            prompt: "Hi",
+            webSearch: "always",
+            plays: { kind: "start" },
+          },
+        ],
+      }),
+    );
+    expect(shared.segments[0].webSearch).toBe("disabled");
+  });
+
+  it("rejects documents that are not a valid shared show", () => {
+    const invalid = "providers.ai_radio.validation.invalid_import_file";
+    const base = {
+      kind: "ai_radio_show",
+      version: 1,
+      name: "Show",
+      segments: [{ name: "Intro", prompt: "Hi", plays: { kind: "start" } }],
+    };
+    const rejects = [
+      "not json at all",
+      "[]",
+      JSON.stringify({ ...base, kind: "something_else" }),
+      JSON.stringify({ ...base, version: 2 }),
+      JSON.stringify({ ...base, name: "   " }),
+      JSON.stringify({ ...base, segments: [] }),
+      JSON.stringify({
+        ...base,
+        segments: [{ name: "Intro", prompt: "", plays: { kind: "start" } }],
+      }),
+      JSON.stringify({
+        ...base,
+        segments: [
+          { name: "Intro", prompt: { evil: true }, plays: { kind: "start" } },
+        ],
+      }),
+      JSON.stringify({
+        ...base,
+        segments: [{ name: "Intro", prompt: "Hi", plays: { kind: "never" } }],
+      }),
+      JSON.stringify({
+        ...base,
+        segments: [{ name: "Intro", prompt: "x".repeat(8001) }],
+      }),
+      JSON.stringify({
+        ...base,
+        segments: Array.from({ length: 51 }, () => ({
+          name: "Intro",
+          prompt: "Hi",
+          plays: { kind: "start" },
+        })),
+      }),
+    ];
+    for (const payload of rejects) {
+      expect(() => parseSharedShow(payload)).toThrowError(invalid);
+    }
+  });
+});
+
+describe("sharedShowFileName", () => {
+  it("slugifies the show name", () => {
+    expect(sharedShowFileName("Late Night Deep Cuts")).toBe(
+      "late_night_deep_cuts.ai-radio-show.json",
+    );
   });
 });